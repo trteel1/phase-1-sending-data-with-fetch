ceb37445e4ad692917859ff5b6280b06efaf0928
trteel1 <tateelamino@gmail.com>
2023-01-26 20:06:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const submitData = function(userName, userEmail) {
+    const userInfo = {
+        name: userName,
+        email: userEmail
+    };
+    try {
+        return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(userInfo)
+        }).then((response) => {
+            return response.json();
+        }).then((jsonData) => {
+            return document.body.innerHTML = jsonData.id
+        }).catch((err) => {
+            document.body.innerHTML = err.message;
+        });
+    } catch(err) {
+        console.log('Error fetching dada from servers');
+    }
+}
+
+submitData('Abdo', 'abdo@gmail.com');
